2c811d3c4e508fa66882317dab4f1d8260364478
horita-yuya <horita_yuya@cyberagent.co.jp>
2019-01-15 11:44:34 +0900

    clean up

--- KotlinNativeiOS/sharedNative/src/iosMain/kotlin/actual.kt
+++ KotlinNativeiOS/sharedNative/src/iosMain/kotlin/actual.kt
@@ -1,12 +1,6 @@
 package com.github.horita_yuya
 
-import kotlinx.coroutines.*
 import platform.Foundation.NSDate
-import platform.darwin.dispatch_async
-import platform.darwin.dispatch_get_main_queue
-import kotlin.coroutines.CoroutineContext
-import kotlin.native.concurrent.freeze
-import kotlin.native.concurrent.isFrozen
 
 actual fun currentTime(): String {
     return NSDate().toString()
